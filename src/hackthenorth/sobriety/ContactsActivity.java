e48873a344889f30d64a3d3a15f88d786e4f9266
Cassie Corey <cassianjanay@gmail.com>
2014-09-20 04:19:19 -0400

    fixed min level

--- src/hackthenorth/sobriety/ContactsActivity.java
+++ src/hackthenorth/sobriety/ContactsActivity.java
@@ -1,5 +1,6 @@
 package hackthenorth.sobriety;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,7 @@ public class ContactsActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contacts);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
@@ -25,10 +27,12 @@ public class ContactsActivity extends ActionBarActivity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+		switch(item.getItemId()) {
+			case R.id.action_settings: return true;
+			case android.R.id.home:
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+			}
 		return super.onOptionsItemSelected(item);
 	}
 }
