33310fb844e777aa57f9ea276b8ba217468a0603
Cassie Corey <cassianjanay@gmail.com>
2014-09-20 02:08:10 -0400

    added calculator activity

--- gen/hackthenorth/sobriety/R.java
+++ gen/hackthenorth/sobriety/R.java
@@ -31,15 +31,18 @@ public final class R {
         public static final int sober_drivers=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_launch=0x7f030000;
+        public static final int activity_calculator=0x7f030000;
+        public static final int activity_launch=0x7f030001;
     }
     public static final class menu {
-        public static final int launch=0x7f070000;
+        public static final int calculator=0x7f070000;
+        public static final int launch=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_calculator=0x7f050003;
     }
     public static final class style {
         /** 
