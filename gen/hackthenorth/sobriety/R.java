b0ae914f419064f3da5b7ac5acb51c575403102a
Cassie Corey <cassianjanay@gmail.com>
2014-09-20 02:22:24 -0400

    calculator xmls built

--- gen/hackthenorth/sobriety/R.java
+++ gen/hackthenorth/sobriety/R.java
@@ -25,14 +25,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080006;
         public static final int calculator=0x7f080001;
+        public static final int drink_picker=0x7f080003;
         public static final int get_home=0x7f080002;
         public static final int sober_drivers=0x7f080000;
+        public static final int weight_picker=0x7f080005;
+        public static final int weight_type=0x7f080004;
     }
     public static final class layout {
         public static final int activity_calculator=0x7f030000;
         public static final int activity_launch=0x7f030001;
+        public static final int calculator_count=0x7f030002;
+        public static final int calculator_sex=0x7f030003;
+        public static final int calculator_time=0x7f030004;
+        public static final int calculator_weight=0x7f030005;
     }
     public static final class menu {
         public static final int calculator=0x7f070000;
